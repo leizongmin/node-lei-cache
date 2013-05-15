400da76ba68e89f6a9fd0d148bfcd02101444687
Lei Zongmin <leizongmin@gmail.com>
2013-05-15 13:26:43 +0800

    使用yuidoc

---
+++ doc/api.js
@@ -0,0 +1,17 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Cache"
+    ],
+    "modules": [
+        "lei-cache"
+    ],
+    "allModules": [
+        {
+            "displayName": "lei-cache",
+            "name": "lei-cache",
+            "description": "Memory Cache Pool"
+        }
+    ]
+} };
+});
